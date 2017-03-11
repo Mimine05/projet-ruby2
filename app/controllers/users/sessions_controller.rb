5191663db25b8e566a086452a370ca0451d7df1b
Mimine05 <mariineeee-e@live.fr>
2017-03-11 17:55:05 +0100

    create profil

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -13,7 +13,7 @@ class Users::SessionsController < Devise::SessionsController
 
     # DELETE /resource/sign_out
     def destroy
-        super
+        # super
     end
 
     protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
     get 'users/profil'
     get 'user' => 'users#profil'
-    delete 'users/sign_out' => 'users/session#destroy'
-    resources :users do 
-        resources :formations
-        resources :experiences
-        resources :interets
-    end
 
     resources :interets
     resources :experiences
     resources :formations
     get 'home/index'
 
-    devise_for :users, controllers: { sessions: 'users/sessions' }
+    devise_for :users do
+        resources :users do 
+            resources :formations
+            resources :experiences
+            resources :interets
+        end
+    end
 
     root to: 'home#index'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
